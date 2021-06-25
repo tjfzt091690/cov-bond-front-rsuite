047237303aade2fe99fbb88c5c731bf6e94426ad
tjfzt091690 <976755393@qq.com>
2021-06-25 20:30:13 +0800

    第一版本

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DoubleLow from './DoubleLow';
+import './styles/index.less';
+ReactDOM.render(
+  <React.StrictMode>
+    <DoubleLow />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
